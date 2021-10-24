4af5aff99584660755b92d15dc24e22cf09a4b2e
ahelmholz <austin.helmholz@gmail.com>
2021-10-23 22:39:07 -0700

    Register Page

---
+++ data.ts
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    id: 1,
+    name: "Austin",
+  },
+  {
+    id: 2,
+    name: "David",
+  },
+  {
+    id: 3,
+    name: "Valerie",
+  },
+  {
+    id: 4,
+    name: "Rebeka",
+  },
+  {
+    id: 5,
+    name: "Guest",
+  },
+];
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,9 +1,65 @@
 import React from "react";
+import { users } from "../data";
+
+import styles from "./register.module.css";
 
 interface Props {}
 
 const Register = (props: Props) => {
-  return <div>Registar</div>;
+  const [user, setUser] = React.useState<null | {
+    userId: number;
+    userName: string;
+  }>(null);
+
+  const [newUserId, setNewUserId] = React.useState<null | number>(null);
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    const newUserInfo = users.find((user) => user.id === newUserId);
+    const userIdString = newUserInfo ? newUserInfo.id.toString() : null;
+    localStorage.setItem("userName", newUserInfo?.name || "");
+    localStorage.setItem("userId", userIdString ?? "");
+    alert("New user set");
+    setUser({
+      userId: newUserInfo?.id ?? 0,
+      userName: newUserInfo?.name || "",
+    });
+  };
+
+  const onChange = (event: any) => {
+    setNewUserId(parseInt(event.target.value));
+  };
+
+  React.useEffect(() => {
+    const userName = localStorage.getItem("userName");
+    const userId = localStorage.getItem("userId");
+    if (userName && userId) {
+      setUser({
+        userName,
+        userId: parseInt(userId, 10),
+      });
+    }
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {user && (
+        <p>
+          Currently signed in as: <b>{user.userName}</b>
+        </p>
+      )}
+      <form onSubmit={onSubmit}>
+        <select name='user' onChange={onChange}>
+          {users.map(({ id, name }) => (
+            <option value={id} key={id}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <button type='submit'>Change User</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
