f4da23f4928a867e94034aa9fdfd37ce4b9067fc
ahelmholz <austin.helmholz@gmail.com>
2021-10-23 23:44:10 -0700

    Require Login Component

--- components/RequireLogin/index.tsx
+++ components/RequireLogin/index.tsx
@@ -1,8 +1,19 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 interface Props {}
 
 const RequireLogin: React.FC<Props> = (props) => {
+  const router = useRouter();
+  React.useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (!userId) {
+      alert(
+        "You must register first. You will be redirected to the register page. Please rescan your QR code once signed in."
+      );
+      router.replace("/register");
+    }
+  }, []);
   return <>{props.children}</>;
 };
 
