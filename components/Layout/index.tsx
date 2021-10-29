3c090a2436c64222e85443d0c6c7e1a62d35b57a
David Taylor <officialdavidtaylor@gmail.com>
2021-10-28 23:22:41 -0700

    implemented header

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Header from "../Header";
 import styles from "./layout.module.css";
 
 interface Props {}
 
 const Layout: React.FC<Props> = (props) => {
-  return <main className={styles.container}>{props.children}</main>;
+  return (
+    <>
+      <Header />
+      <main className={styles.container}>{props.children}</main>;
+    </>
+  )
 };
 
 export default Layout;
