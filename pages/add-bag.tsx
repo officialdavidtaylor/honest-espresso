b70c23d87417c9d1da76e32567cebf435dd7c8b2
David Taylor <officialdavidtaylor@gmail.com>
2021-11-04 08:55:42 -0700

    created add-bag form and basic styling

--- pages/add-bag.tsx
+++ pages/add-bag.tsx
@@ -1,9 +1,39 @@
 import React from "react";
 
-interface Props {}
+import styles from "./add-bag.module.css";
+
+interface Props { }
 
 const AddBagPage = (props: Props) => {
-  return <div></div>;
+  return (
+    <div className={styles.formContainer}>
+      <form>
+        <label>Roaster:</label><br />
+        <input type="text" name="roaster" /><br />
+
+        <label>Roast Name:</label><br />
+        <input type="text" name="roastName" /><br />
+
+        <label>Roast Level:</label><br />
+        <select className="selectList">
+          <option>Medium</option>
+          <option>Light</option>
+          <option>Dark</option>
+        </select><br />
+
+        <label>Shot Weight (g):</label><br />
+        <input type="text" name="shotWeight" defaultValue="17.0" /><br />
+
+        <label>Bag Cost: <span style={{ color: "gray" }}>(post-tax)</span></label><br />
+        <input className={styles.bagCost} type="text" name="bagCost" pattern="^\d|\d+.|\d+(.\d{1,2})?$" /><br />
+
+        <label>Shot Prep Notes:</label><br />
+        <input type="text" name="prepNotes" /><br />
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default AddBagPage;
