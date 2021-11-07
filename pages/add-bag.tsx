fdde84c66a9459c8a2a9d3fd59a0987958aa8c07
ahelmholz <austin.helmholz@gmail.com>
2021-11-06 21:59:18 -0700

    Add Bag Form

--- pages/add-bag.tsx
+++ pages/add-bag.tsx
@@ -1,37 +1,150 @@
 import React from "react";
+import { useMutation, gql } from "@apollo/client";
+import { useForm } from "react-hook-form";
 
 import styles from "./add-bag.module.css";
 
-interface Props { }
+const AddBagMutation = gql`
+  mutation InsertNewBag(
+    $imageUrl: String!
+    $bagOwnerId: Int!
+    $bagCost: numeric!
+    $roastName: String!
+    $roaster: String!
+    $bagWeight: Int!
+    $roastLevel: String!
+  ) {
+    insert_coffee_bag_one(
+      object: {
+        image_url: $imageUrl
+        roaster: $roaster
+        roast_name: $roastName
+        price_paid: $bagCost
+        weight_in_grams: $bagWeight
+        owner_user_id: $bagOwnerId
+        roast_level: $roastLevel
+      }
+    ) {
+      id
+      roaster
+    }
+  }
+`;
+
+interface Props {}
+
+interface FormValues {
+  imageUrl: string;
+  roaster: string;
+  roastName: string;
+  roastLevel: string;
+  bagCost: number;
+  bagWeight: string;
+  bagOwnerId: string;
+}
+
+const style = {
+  width: "100%",
+  margin: "10px 0",
+};
 
 const AddBagPage = (props: Props) => {
+  const [addBagMutation, addBagProperties] = useMutation(AddBagMutation);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: { roastLevel: "medium" },
+  });
+
+  const onSubmit = async (values: FormValues) => {
+    await addBagMutation({ variables: values });
+  };
+  console.log({ addBagProperties });
+
   return (
     <div className={styles.formContainer}>
-      <form>
-        <label>Roaster:</label><br />
-        <input type="text" name="roaster" /><br />
-
-        <label>Roast Name:</label><br />
-        <input type="text" name="roastName" /><br />
-
-        <label>Roast Level:</label><br />
-        <select className="selectList" name="roastLevel">
-          <option>Medium</option>
-          <option>Light</option>
-          <option>Dark</option>
-        </select><br />
-
-        <label>Shot Weight (g):</label><br />
-        <input type="text" name="shotWeight" defaultValue="17.0" /><br />
-
-        <label>Bag Cost: <span style={{ color: "gray" }}>(post-tax)</span></label><br />
-        <input className={styles.bagCost} type="text" name="bagCost" pattern="^\d|\d+.|\d+(.\d{1,2})?$" /><br />
-
-        <label>Shot Prep Notes:</label><br />
-        <input type="text" name="prepNotes" /><br />
-
-        <input type="submit" value="Submit" />
+      <h2>Add Bag Form</h2>
+      <form onSubmit={handleSubmit(onSubmit)} style={style}>
+        <label>Bag Image Url:</label>
+        <br />
+        <input
+          type='url'
+          {...register("imageUrl", { required: true })}
+          style={style}
+        />
+        <br />
+        <label>Roaster:</label>
+        <br />
+        <input
+          type='text'
+          {...register("roaster", { required: true })}
+          style={style}
+        />
+        <br />
+        <label>Roast Name:</label>
+        <br />
+        <input
+          type='text'
+          {...register("roastName", { required: true })}
+          style={style}
+        />
+        <br />
+        <label>Roast Level:</label>
+        <br />
+        <select
+          className='selectList'
+          style={style}
+          {...register("roastLevel", { required: true })}
+        >
+          <option value='light'>Light</option>
+          <option value='medium'>Medium</option>
+          <option value='dark'>Dark</option>
+        </select>
+        <br />
+        <label>
+          Bag Cost: <span style={{ color: "gray" }}>(post-tax)</span>
+        </label>
+        <br />
+        <input
+          type='text'
+          style={style}
+          {...register("bagCost", { required: true })}
+        />
+        <br />
+        <label>Bag Weight(grams):</label>
+        <br />
+        <input
+          type='number'
+          style={style}
+          {...register("bagWeight", { required: true })}
+        />
+        <br />
+        <label>Bag Owner</label>
+        <br />
+        <select style={style} {...register("bagOwnerId", { required: true })}>
+          <option value='1'>Austin</option>
+          <option value='2'>David</option>
+        </select>
+        <br />
+        <br />
+        <button
+          type='submit'
+          style={style}
+          disabled={addBagProperties.loading || addBagProperties.called}
+        >
+          {addBagProperties.loading ? "Loading..." : "Add Bag"}
+        </button>
       </form>
+      {Object.values(errors).reduce(
+        (acc: any, cur) => (acc ? acc : cur ? cur : null),
+        null
+      ) &&
+        addBagProperties.error && (
+          <p style={{ color: "red" }}>Something went wrong :(</p>
+        )}
     </div>
   );
 };
