5b1b8adc1478a4c8fccfd625c61d980526efeb30
ahelmholz <austin.helmholz@gmail.com>
2021-11-07 00:53:15 -0700

    Added Nav Links to Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+
+const linkStyles = {
+  background: "#909CAC",
+  margin: "12px 0",
+  padding: "12px",
+  borderRadius: "12px",
+  display: "flex",
+  justifyContent: "center",
+};
 
 const Home: NextPage = () => {
-  return <div>hello honest espresso</div>;
+  return (
+    <div style={{ width: "calc(100% - 40px)", padding: "0 20px" }}>
+      <Link href='/report' passHref>
+        <a>
+          <div style={linkStyles}>Report</div>
+        </a>
+      </Link>
+      <Link href='/add-bag' passHref>
+        <a>
+          <div style={linkStyles}>Add Bag</div>
+        </a>
+      </Link>
+      <Link href='/register' passHref>
+        <a>
+          <div style={linkStyles}>Register</div>
+        </a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
